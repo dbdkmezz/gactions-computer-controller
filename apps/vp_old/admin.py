9bbb871900340d54f58b809710b8a2553aa0acfd
Paul Carver <dbdkmezz@gmail.com>
2017-12-02 18:00:09 +0000

    moved video_player into submodule

--- apps/vp_old/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import VideoFolder, Video
-
-
-admin.site.register(Video)
-admin.site.register(VideoFolder)
--- apps/vp_old/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class VideoPlayerConfig(AppConfig):
-    name = 'apps.video_player'
